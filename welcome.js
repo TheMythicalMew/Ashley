541fe7ba6f01d4f6f7ccd4390c0f51b55debd628
Glitch (ashle) <none>
2018-08-24 17:37:44 +0000

    Code fix

--- welcome.js
+++ welcome.js
@@ -5,16 +5,6 @@ const app = express();
 const http = require('http');
 const config = require('./config.json');
 
-/*app.get("/", (request, response) => {
-    console.log(Date.now() + " Ping Received");
-    response.sendStatus(200);
-      });
-     app.listen(process.env.PORT);
-      setInterval(() => {
-        http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-      }, 280000);
-*/
-
 client.on("ready", () => {
     console.log('Welcome and leaving reggie!');
 })
