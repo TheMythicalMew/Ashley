f819312bec801a68278fafe8847a570b0004b4fb
Mew <thedalekmoduniversemew@gmail.com>
2018-08-24 09:22:24 +0100

    ooops

--- cats.js
+++ cats.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const superagent = require("superagent");
+const config = require('./config.json');
 
 client.on("ready", () => {
   console.log('Meow, kitty cats reggie!');
